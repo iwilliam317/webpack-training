ee786ca27347f9d6e3453184d6550849932d6ae7
william <willshinji@gmail.com>
2018-12-03 20:18:03 -0200

    react example

---
+++ ex/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = () => (
+    <h1>Hello!</h1>
+)
+
+ReactDOM.render(<Hello />, document.getElementById('root'))
+console.log('It is compiled!')
